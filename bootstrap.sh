95e729d3eaffd528cb8b237efd7e2e0142c67107
Douglas Holt <dholt@nvidia.com>
2017-07-19 15:10:24 -0600

    workaround for out-of-date docker role

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,9 @@ sudo apt-get -y install ansible
 # Add nvidia-docker role from Ansible Galaxy
 ansible-galaxy install ryanolson.nvidia-docker --roles-path=/tmp/roles
 
+# Temporary workaround for docker role
+sed -i 's/docker-engine/docker-ce/g' /tmp/roles/ryanolson.docker/tasks/main.yml
+
 # Write playbook
 f=$(mktemp)
 cat <<EOF > $f
