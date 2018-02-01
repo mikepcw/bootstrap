f30687de45137d83af6141bdca5aca286494537a
Douglas Holt <dholt@nvidia.com>
2018-01-31 18:24:54 -0700

    update for different docker role and nvidia-docker2

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,8 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
 
-# Add nvidia-docker role from Ansible Galaxy
-ansible-galaxy install ryanolson.nvidia-docker --roles-path=/tmp/roles
-
-# Temporary workarounds for docker role
-sed -i 's/docker-engine/docker-ce/g' /tmp/roles/ryanolson.docker/tasks/main.yml
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Add docker role from Ansible Galaxy
+ansible-galaxy install angstwad.docker_ubuntu --roles-path=/tmp/roles
 
 # Write playbook
 f=$(mktemp)
@@ -26,15 +22,19 @@ cat <<EOF > $f
   become: true
   become_method: sudo
   roles:
-    - role: 'ryanolson.nvidia-docker'
+    - angstwad.docker_ubuntu
   tasks:
     - name: docker | add user to docker group
       user: name=$USER groups=docker append=yes
+    - name: cuda | apt key
+      apt_key:
+        url: http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+        state: present
     - name: cuda | repo
       apt: deb=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
       when: (ansible_distribution == 'Ubuntu' and ansible_distribution_version == '14.04')
     - name: cuda | repo
-      apt: deb=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+      apt: deb=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
       when: (ansible_distribution == 'Ubuntu' and ansible_distribution_version == '16.04')
     - name: cuda | install prereqs
       apt: name={{ item }} state=latest update_cache=yes cache_valid_time=600
@@ -48,8 +48,26 @@ cat <<EOF > $f
       when: (ansible_distribution == 'Ubuntu')
       with_items:
         - cuda-drivers
-    - name: nvidia-docker | service
-      service: name=nvidia-docker state=restarted enabled=yes
+    - name: nvidia-docker | apt key
+      apt_key:
+        url: https://nvidia.github.io/nvidia-docker/gpgkey
+        state: present
+    - name: nvidia-docker | apt repo
+      apt_repository:
+        repo: "{{ item }}"
+        state: present
+        filename: 'nvidia-docker'
+        update_cache: yes
+      with_items:
+        - "deb https://nvidia.github.io/libnvidia-container/ubuntu16.04/amd64 /"
+        - "deb https://nvidia.github.io/nvidia-container-runtime/ubuntu16.04/amd64 /"
+        - "deb https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64 /"
+    - name: nvidia-docker | install
+      apt:
+        name: nvidia-docker2
+        state: latest
+    - name: docker | restart service
+      service: name=docker state=restarted enabled=yes
 EOF
 
 # Execute playbook
